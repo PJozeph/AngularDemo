165aa9102144d8009fd8fa7b7570654e6b963b54
Jozsef Pallagi <jozsefpallagi@192.168.1.187>
2020-03-29 08:59:09 +0200

    code format

--- recepie-app/src/app/shopping-list/shopping-list.component.ts
+++ recepie-app/src/app/shopping-list/shopping-list.component.ts
@@ -16,7 +16,7 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  addIngridient(ingridient) {
+  addIngridient(ingridient : Ingredient) {
      this.ingredients.push(ingridient);
   }
 }
