81d7e6741da7a7dbb78b6382a85ca98665a6fc85
Jozsef Pallagi <jozsefpallagi@jozsefs-mbp.t.hu>
2020-04-08 09:36:45 +0200

    observable demo

---
+++ observable-start/src/app/home/home.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstSubscription: Subscription;
+  constructor() { }
+
+  ngOnInit() {
+    // this.firstSubscription =  interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObject = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        if (count === 2) {
+          observer.complete();
+        }
+
+        observer.next(count);
+        if (count > 3 ) {
+          observer.error( new Error(' Counter is greather than 3'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstSubscription = customIntervalObject.pipe( map( (data : number) => { return 'Round ' + (data + 1) } ) ).subscribe(count => {
+      console.log(count);
+    }, error => {
+        alert(error.message);
+    }, ()=> {
+      console.log(' subscribe is complited');
+    } );
+  }
+
+  ngOnDestroy(): void {
+    this.firstSubscription.unsubscribe();
+  }
+
+}
