f3da8d15a8985b90ed9fad8606a85c53f520a2c8
jozsef.pallagi <jozsef.pallagi@ibm.com>
2020-04-25 08:41:22 +0200

    submit form

--- forms-demo/src/app/app.component.ts
+++ forms-demo/src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("formElement", { static: false }) form: NgForm;
+
+  defaultQuestion = "pet";
+  answer = "";
+  genders = ["male", "female"];
+  submitted = false;
+  user = {
+    userName: "",
+    userEmail: "",
+    userGender: "",
+    userQuestionAnswer: "",
+    userSecret: ""
+  };
+
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+
+  onSubmit(formElement: NgForm) {
+    this.submitted = true;
+    console.log(formElement.value.userData)
+    this.user.userEmail = formElement.value.userData.email;
+    this.user.userName = formElement.value.userData.username;
+    this.user.userSecret = formElement.value.userData.questionAnswer;
+    this.user.userSecret = formElement.value.userData.secret;
   }
 }
